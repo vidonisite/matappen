a3c52b27142dea1e2b42cb8d3f38641d35276d65
vidonisite <joni.zernell@proton.me>
2026-09-21 16:09:30 +0200

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./supabaseClient";
+import "./Home.css";
 
 function Home() {
   const [products, setProducts] = useState([]);
